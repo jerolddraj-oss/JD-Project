d1393ef57ea2b956a853732d864af7227bd005a7
jerolddraj-oss <jerold.d.raj@gmail.com>
2026-05-04 18:22:26 +0530

    Delete backend.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "k8-backup"
-    storage_account_name = "terraformtfstateremote"
-    container_name       = "tfstate1979"
-    key                  = "terraform.tfstate1979"
-  }
-}
