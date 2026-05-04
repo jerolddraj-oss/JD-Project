d6e49e1b7cca8d1ae684f8e04f330005291c0095
jerolddraj-oss <jerold.d.raj@gmail.com>
2026-05-04 14:52:40 +0530

    Update variables.tf to include hub_vnet_id variable

--- modules/network-security/variables.tf
+++ modules/network-security/variables.tf
@@ -2,3 +2,7 @@ variable "location" {}
 variable "rg" {}
 variable "firewall_subnet_id" {}
 variable "public_ip_id" {}
+variable "hub_vnet_id" {
+  description = "The ID of the Hub Virtual Network"
+  type        = string
+}
