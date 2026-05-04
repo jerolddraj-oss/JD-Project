b75f9a2da4e4e93399a141f062f8b99e19085a49
jerolddraj-oss <jerold.d.raj@gmail.com>
2026-05-04 23:00:56 +0530

    Update main.tf

--- modules/networking/vnet-peering/main.tf
+++ modules/networking/vnet-peering/main.tf
@@ -1,13 +1,4 @@
-variable "resource_group_name" { type = string }
-variable "location" { type = string }
-variable "vnet_name" { type = string }
-variable "vnet_id" { type = string }
-variable "peer_vnet_name" { type = string }
-variable "peer_vnet_id" { type = string }
-variable "allow_forwarded_traffic" { type = bool default = false }
-variable "allow_gateway_transit" { type = bool default = false }
-variable "use_remote_gateways" { type = bool default = false }
-variable "tags" { type = map(string) }
+
 
 resource "azurerm_virtual_network_peering" "to_peer" {
   name                      = "${var.vnet_name}-to-${var.peer_vnet_name}"
