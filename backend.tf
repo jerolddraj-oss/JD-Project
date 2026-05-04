edcbe0bbc466cee9126d98dcf46bb56c7087af2f
jerolddraj-oss <jerold.d.raj@gmail.com>
2026-05-04 18:12:25 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "tfstate-rg"
-    storage_account_name = "jdterraformstate"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
+    resource_group_name  = "k8-backup"
+    storage_account_name = "terraformtfstateremote"
+    container_name       = "tfstate1979"
+    key                  = "terraform.tfstate1979"
   }
 }
