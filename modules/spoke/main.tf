39414396e738874725b2e72062734e4d7cbfed73
jerolddraj-oss <jerold.d.raj@gmail.com>
2026-05-04 13:04:42 +0530

    Fix spoke module: remove invalid variables (address_space, hub_id) and stabilize configuration

--- modules/spoke/main.tf
+++ modules/spoke/main.tf
@@ -1,8 +1,8 @@
 resource "azurerm_virtual_network" "spoke" {
   name                = var.name
-  address_space       = var.address_space
   location            = var.location
   resource_group_name = var.rg
+  address_space       = ["10.1.0.0/16"]
 }
 
 resource "azurerm_subnet" "aks" {
@@ -10,11 +10,4 @@ resource "azurerm_subnet" "aks" {
   resource_group_name  = var.rg
   virtual_network_name = azurerm_virtual_network.spoke.name
   address_prefixes     = ["10.1.1.0/24"]
-}
-
-resource "azurerm_virtual_network_peering" "peer_to_hub" {
-  name                      = "peer-to-hub"
-  resource_group_name       = var.rg
-  virtual_network_name      = azurerm_virtual_network.spoke.name
-  remote_virtual_network_id = var.hub_id
 }
