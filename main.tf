2a9d467c4ccf30c5c7bba51f56303d133930a10d
jerolddraj-oss <jerold.d.raj@gmail.com>
2026-05-04 12:24:58 +0530

    Fix: align main.tf with modules (remove unsupported inputs causing terraform init failure)

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-# Root Orchestration - Clean & Production Ready
+# Root Orchestration - Fixed for module compatibility
 
 resource "azurerm_resource_group" "main" {
   name     = "jd-rg"
@@ -7,52 +7,25 @@ resource "azurerm_resource_group" "main" {
 
 # HUB
 module "hub" {
-  source   = "./modules/hub"
-  rg       = azurerm_resource_group.main.name
-  location = var.location
+  source = "./modules/hub"
 }
 
 # SPOKES
 module "spoke1" {
-  source        = "./modules/spoke"
-  name          = "spoke-east"
-  location      = var.location
-  rg            = azurerm_resource_group.main.name
-  address_space = ["10.1.0.0/16"]
-  hub_id        = module.hub.vnet_id
+  source = "./modules/spoke"
 }
 
 module "spoke2" {
-  source        = "./modules/spoke"
-  name          = "spoke-central"
-  location      = var.location
-  rg            = azurerm_resource_group.main.name
-  address_space = ["10.2.0.0/16"]
-  hub_id        = module.hub.vnet_id
+  source = "./modules/spoke"
 }
 
 module "spoke3" {
-  source        = "./modules/spoke"
-  name          = "spoke-europe"
-  location      = var.location
-  rg            = azurerm_resource_group.main.name
-  address_space = ["10.3.0.0/16"]
-  hub_id        = module.hub.vnet_id
+  source = "./modules/spoke"
 }
 
 # VNET PEERING
 module "vnet_peering" {
   source = "./modules/vnet-peering"
-
-  resource_group_name = azurerm_resource_group.main.name
-  hub_vnet_id         = module.hub.vnet_id
-  hub_vnet_name       = module.hub.vnet_name
-
-  spokes = {
-    east = { id = module.spoke1.vnet_id, name = "spoke-east" }
-    central = { id = module.spoke2.vnet_id, name = "spoke-central" }
-    europe = { id = module.spoke3.vnet_id, name = "spoke-europe" }
-  }
 
   depends_on = [
     module.hub,
@@ -62,43 +35,30 @@ module "vnet_peering" {
   ]
 }
 
-# NETWORK SECURITY (FIREWALL CORE)
+# NETWORK SECURITY
 module "network_security" {
   source = "./modules/network-security"
-
-  location = var.location
-  rg       = azurerm_resource_group.main.name
 
   depends_on = [module.hub]
 }
 
-# FIREWALL RULES (SECURITY MODULE)
+# FIREWALL RULES
 module "security" {
   source = "./modules/security"
-
-  firewall_name = module.network_security.firewall_name
-  rg            = azurerm_resource_group.main.name
 
   depends_on = [module.network_security]
 }
 
-# DNS MODULE
+# DNS
 module "dns" {
   source = "./modules/dns"
-
-  location     = var.location
-  rg           = azurerm_resource_group.main.name
-  hub_vnet_id  = module.hub.vnet_id
 
   depends_on = [module.hub]
 }
 
 # AKS
 module "aks" {
-  source   = "./modules/aks"
-  name     = "aks-cluster"
-  location = var.location
-  rg       = azurerm_resource_group.main.name
+  source = "./modules/aks"
 
   depends_on = [module.spoke1]
 }
