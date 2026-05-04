69409716c2e99e363c0030fdda4dde12dc091995
jerolddraj-oss <jerold.d.raj@gmail.com>
2026-05-04 22:59:18 +0530

    Update main.tf

--- modules/aks/main-cluster/main.tf
+++ modules/aks/main-cluster/main.tf
@@ -1,15 +1,3 @@
-variable "resource_group_name" { type = string }
-variable "location" { type = string }
-variable "name" { type = string }
-variable "dns_prefix" { type = string }
-variable "vnet_subnet_id" { type = string }
-variable "kubernetes_version" { type = string }
-variable "node_count" { type = number }
-variable "node_vm_size" { type = string }
-variable "enable_azure_ad_integration" { type = bool default = true }
-variable "log_analytics_workspace_id" { type = string }
-variable "tags" { type = map(string) }
-
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = var.name
   location            = var.location
@@ -18,15 +6,15 @@ resource "azurerm_kubernetes_cluster" "aks" {
   kubernetes_version  = var.kubernetes_version
 
   default_node_pool {
-    name       = "agentpool"
-    node_count = var.node_count
-    vm_size    = var.node_vm_size
-    vnet_subnet_id = var.vnet_subnet_id
-    availability_zones = ["1","2","3"]
-    type = "VirtualMachineScaleSets"
+    name               = "agentpool"
+    node_count         = var.node_count
+    vm_size            = var.node_vm_size
+    vnet_subnet_id     = var.vnet_subnet_id
+    availability_zones = ["1", "2", "3"]
+    type               = "VirtualMachineScaleSets"
     enable_auto_scaling = true
-    min_count = 1
-    max_count = 5
+    min_count          = 1
+    max_count          = 5
   }
 
   identity {
@@ -34,10 +22,10 @@ resource "azurerm_kubernetes_cluster" "aks" {
   }
 
   network_profile {
-    network_plugin = "azure"
-    network_policy = "azure"
+    network_plugin    = "azure"
+    network_policy    = "azure"
     load_balancer_sku = "standard"
-    outbound_type = "userDefinedRouting"
+    outbound_type     = "userDefinedRouting"
   }
 
   role_based_access_control {
@@ -46,7 +34,8 @@ resource "azurerm_kubernetes_cluster" "aks" {
 
   azure_active_directory {
     managed = var.enable_azure_ad_integration
-    # If integrating with existing AAD, additional fields (admin_group_object_ids) can be provided
+    # For full AAD integration, supply admin_group_object_ids via variables and uncomment below:
+    # admin_group_object_ids = var.admin_group_object_ids
   }
 
   addon_profile {
