fabba2e9829846d7119b62363a0aa56c15d702b7
jerolddraj-oss <jerold.d.raj@gmail.com>
2026-05-04 14:49:31 +0530

    Update main.tf with new Azure resources configuration.

--- modules/hub/main.tf
+++ modules/hub/main.tf
@@ -11,3 +11,11 @@ resource "azurerm_subnet" "firewall" {
   virtual_network_name = azurerm_virtual_network.hub.name
   address_prefixes     = ["10.0.1.0/24"]
 }
+
+resource "azurerm_public_ip" "firewall" {
+  name                = "jd-firewall-pip"
+  location            = var.location
+  resource_group_name = var.rg
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
