b0ca5ccb1b18998847005aa6da48d548e44b70f8
jerolddraj-oss <jerold.d.raj@gmail.com>
2026-05-04 22:45:54 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -3,21 +3,26 @@ output "resource_group_name" {
 }
 
 output "vmss_a_public_ip" {
-  value = module.vmss_a.public_ip
+  value       = module.vmss_a.public_ip
+  description = "Public IP address for VMSS A"
 }
 
 output "vmss_b_public_ip" {
-  value = module.vmss_b.public_ip
+  value       = module.vmss_b.public_ip
+  description = "Public IP address for VMSS B"
 }
 
 output "traffic_manager_fqdn" {
-  value = module.traffic_manager.fqdn
+  value       = module.traffic_manager.fqdn
+  description = "Traffic Manager profile FQDN"
 }
 
 output "key_vault_uri" {
-  value = module.key_vault.vault_uri
+  value       = module.key_vault.vault_uri
+  description = "Key Vault URI"
 }
 
 output "aks_cluster_name" {
-  value = module.aks.cluster_name
+  value       = module.aks.cluster_name
+  description = "AKS cluster name"
 }
