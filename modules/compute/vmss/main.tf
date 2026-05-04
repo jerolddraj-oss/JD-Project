ab3748e6dcdbff4a1b9acde20768baf15eee4e9b
jerolddraj-oss <jerold.d.raj@gmail.com>
2026-05-04 23:00:20 +0530

    Update main.tf

--- modules/compute/vmss/main.tf
+++ modules/compute/vmss/main.tf
@@ -1,12 +1,4 @@
-variable "resource_group_name" { type = string }
-variable "location" { type = string }
-variable "name" { type = string }
-variable "instance_count" { type = number }
-variable "vm_size" { type = string }
-variable "subnet_id" { type = string }
-variable "zones" { type = list(string) default = [] }
-variable "tags" { type = map(string) }
-variable "log_analytics_workspace_id" { type = string }
+
 
 resource "random_string" "admin" {
   length  = 12
