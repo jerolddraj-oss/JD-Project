1b4cd01ef9ba4c9b4b09ed2bcf425b93757bfbcd
jerolddraj-oss <jerold.d.raj@gmail.com>
2026-05-04 22:51:23 +0530

    Create variables.tf

---
+++ modules/security/key-vault/variables.tf
@@ -0,0 +1,24 @@
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "name" {
+  type = string
+}
+
+variable "tenant_id" {
+  type = string
+}
+
+variable "object_ids" {
+  type    = list(string)
+  default = []
+}
+
+variable "tags" {
+  type = map(string)
+}
