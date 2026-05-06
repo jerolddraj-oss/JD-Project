8cc83114093e434815ba37750f19de5d314c5b7d
jerolddraj-oss <jerold.d.raj@gmail.com>
2026-05-06 14:07:11 +0530

    Refine Terraform variable defaults

--- RDS-Project/terraform/variables.tf
+++ RDS-Project/terraform/variables.tf
@@ -1,7 +1,17 @@
-variable "resource_group_name" {}
-variable "location" {}
-variable "admin_username" {}
+variable "resource_group_name" {
+  default = "rg-rds-poc"
+}
+
+variable "location" {
+  default = "East US"
+}
+
+variable "admin_username" {
+  default = "azureadmin"
+}
+
 variable "admin_password" {
   sensitive = true
 }
+
 variable "tenant_id" {}
