c74396d1458c26e7bf363892733da9b04e8a9d15
jerolddraj-oss <jerold.d.raj@gmail.com>
2026-05-06 12:12:18 +0530

    Delete backend.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = var.backend_resource_group
-    storage_account_name = var.backend_storage_account
-    container_name       = var.backend_container
-    key                  = var.backend_key
-  }
-}
